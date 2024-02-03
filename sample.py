b43ac55627a44d300160a98c8c7de16a1f4c0957
Nishith Savla <nishithsavla005@gmail.com>
2024-02-03 22:26:00 +0530

    Create a fastapi server

--- sample.py
+++ sample.py
@@ -1,77 +1,61 @@
-import torch
-import argparse
+from contextlib import asynccontextmanager
+from io import BytesIO
+
+import uvicorn
 from PIL import Image
-from moondream import Moondream, detect_device
-from queue import Queue
-from threading import Thread
+from fastapi import FastAPI, UploadFile, File, Form
+from fastapi.middleware.cors import CORSMiddleware
 from transformers import (
-    TextIteratorStreamer,
-    CodeGenTokenizerFast as Tokenizer,
+    CodeGenTokenizerFast as Tokenizer, PreTrainedTokenizerBase,
 )
-import re
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--image", type=str, required=True)
-    parser.add_argument("--prompt", type=str, required=False)
-    parser.add_argument("--cpu", action="store_true")
-    args = parser.parse_args()
+from moondream import Moondream, detect_device
 
-    if args.cpu:
-        device = torch.device("cpu")
-        dtype = torch.float32
-    else:
-        device, dtype = detect_device()
-        if device != torch.device("cpu"):
-            print("Using device:", device)
-            print("If you run into issues, pass the `--cpu` flag to this script.")
-            print()
+model: Moondream | None = None
+tokenizer: PreTrainedTokenizerBase | None = None
 
-    image_path = args.image
-    prompt = args.prompt
+
+def load_model():
+    device, dtype = detect_device()
 
     model_id = "vikhyatk/moondream1"
     tokenizer = Tokenizer.from_pretrained(model_id)
-    moondream = Moondream.from_pretrained(model_id).to(device=device, dtype=dtype)
-    moondream.eval()
+    model = Moondream.from_pretrained(model_id).to(device=device, dtype=dtype)
+    model.eval()
 
-    image = Image.open(image_path)
+    return model, tokenizer
+
+
+@asynccontextmanager
+async def lifespan(_: FastAPI):
+    # On startup
+    global model, tokenizer
+    model, tokenizer = load_model()
+    yield
+
+
+app = FastAPI(lifespan=lifespan)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=['*'],
+    allow_headers=['*'],
+)
+
+
+def evaluate_image(moondream, tokenizer, image, prompt):
     image_embeds = moondream.encode_image(image)
 
-    if prompt is None:
-        chat_history = ""
+    return moondream.answer_question(image_embeds, prompt, tokenizer)
 
-        while True:
-            question = input("> ")
 
-            result_queue = Queue()
+@app.post("/evaluate/")
+async def evaluate(image: UploadFile = File(...), prompt: str = Form(...)):
+    img = Image.open(BytesIO(await image.read()))
+    result = evaluate_image(model, tokenizer, img, prompt)
+    return {"result": result}
 
-            streamer = TextIteratorStreamer(tokenizer, skip_special_tokens=True)
 
-            # Separate direct arguments from keyword arguments
-            thread_args = (image_embeds, question, tokenizer, chat_history)
-            thread_kwargs = {"streamer": streamer, "result_queue": result_queue}
-
-            thread = Thread(
-                target=moondream.answer_question,
-                args=thread_args,
-                kwargs=thread_kwargs,
-            )
-            thread.start()
-
-            buffer = ""
-            for new_text in streamer:
-                buffer += new_text
-                if not new_text.endswith("<") and not new_text.endswith("END"):
-                    print(buffer, end="", flush=True)
-                    buffer = ""
-            print(re.sub("<$", "", re.sub("END$", "", buffer)))
-
-            thread.join()
-
-            answer = result_queue.get()
-            chat_history += f"Question: {question}\n\nAnswer: {answer}\n\n"
-    else:
-        print(">", prompt)
-        answer = moondream.answer_question(image_embeds, prompt, tokenizer)
-        print(answer)
+if __name__ == '__main__':
+    uvicorn.run("sample:app", host='0.0.0.0', port=8001)
